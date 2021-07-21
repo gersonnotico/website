e4cf7fe99657236ca04c4c07dd70df50a9af48d9
gerson <gersonotico@gmail.com>
2021-07-21 15:33:07 +0200

    About pag responsivo

--- src/Components/ReviewsSecion.js
+++ src/Components/ReviewsSecion.js
@@ -24,8 +24,12 @@ function ReviewSection() {
 
 const ReviewSectionStyled = styled.section`
     .reviews{
-        display: flex;
-        ;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 1.5rem;
+        @media screen and (max-width:650px){
+            grid-template-columns: repeat(1, 1fr);
+        }
     }
   
 `;
